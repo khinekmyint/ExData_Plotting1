010ed3da08da11ac7ccebc8e4ea10f6859044526
Khine Woo <2kmyint@gmail.com>
2016-09-25 12:27:26 -0700

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,18 @@
+source("loaddataset.R")
+dataset<-loadDataset()
+
+par(mfrow=c(2,2))
+
+plot(dataset$Time, dataset$Global_active_power,type = "l", xlab = "",  ylab = "Energy sub metering")
+
+plot(dataset$Time, dataset$Voltage,type = "l", xlab = "",  ylab = "Energy sub metering")
+
+plot(dataset$Time, dataset$Sub_metering_1,type = "l", xlab = "",  ylab = "Energy sub metering")
+lines(dataset$Time,dataset$Sub_metering_2,col="red")
+lines(dataset$Time,dataset$Sub_metering_3,col="blue")
+legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty = 1)
+
+plot(dataset$Time, dataset$Global_reactive_power,type = "l", xlab = "",  ylab = "Energy sub metering")
+
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
